44e6c3a52c061b7d3cf0caf1167ae38f855e0491
Callum Jones <callum.o.jones+git@protonmail.com>
2025-06-26 16:14:36 +1000

    Add tests for various buffer lengths and values

--- src/lib.rs
+++ src/lib.rs
@@ -86,12 +86,68 @@ mod tests {
     use crate::{Mute, utils::test_utils::create_test_buffer};
 
     #[test]
-    fn basic_test() {
+    fn mute_buffer_512_long() {
         let sample_init_val = 5.0;
 
         let empty_noise_plug = Mute::default();
 
         let mut real_buffers = vec![vec![sample_init_val; 512]; 2];
+        let mut buffer = create_test_buffer(&mut real_buffers).unwrap();
+
+        // Verify that the buffer is what we expect it to be
+        for samples in buffer.iter_samples() {
+            for sample in samples {
+                assert_eq!(*sample, sample_init_val);
+            }
+        }
+
+        // Now pass the buffer through the processing algorithm, which should
+        // assign every sample value to be 0.
+        empty_noise_plug.process_algorithm(&mut buffer);
+
+        // Verify the updated values
+        for samples in buffer.iter_samples() {
+            for sample in samples {
+                assert_eq!(*sample, 0.0);
+            }
+        }
+    }
+
+    #[test]
+    fn mute_buffer_0_long() {
+        let sample_init_val = 5.0;
+
+        let empty_noise_plug = Mute::default();
+
+        let mut real_buffers = vec![vec![sample_init_val; 0]; 2];
+        let mut buffer = create_test_buffer(&mut real_buffers).unwrap();
+
+        // Verify that the buffer is what we expect it to be
+        for samples in buffer.iter_samples() {
+            for sample in samples {
+                assert_eq!(*sample, sample_init_val);
+            }
+        }
+
+        // Now pass the buffer through the processing algorithm, which should
+        // assign every sample value to be 0.
+        empty_noise_plug.process_algorithm(&mut buffer);
+
+        // Verify the updated values
+        for samples in buffer.iter_samples() {
+            for sample in samples {
+                assert_eq!(*sample, 0.0);
+            }
+        }
+    }
+
+    #[test]
+    fn mute_buffer_1024_long() {
+        let sample_init_val = 34.0;
+
+        let empty_noise_plug = Mute::default();
+
+        let mut real_buffers = vec![vec![sample_init_val; 1024]; 2];
         let mut buffer = create_test_buffer(&mut real_buffers).unwrap();
 
         // Verify that the buffer is what we expect it to be
