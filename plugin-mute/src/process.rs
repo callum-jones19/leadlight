3e9348f8b2a7d9cfa466962f46f0964dcfc58f22
Callum Jones <callum.o.jones+git@protonmail.com>
2025-07-25 16:28:25 +1000

    Add documentation to plugin-mute process.rs

--- plugin-mute/src/process.rs
+++ plugin-mute/src/process.rs
@@ -1,5 +1,9 @@
 use nih_plug::buffer::Buffer;
 
+/// The mathematical algorithm applied to the mutable input buffer.
+/// This takes in a generic buffer, and iterates over the samples in
+/// each channel. For each sample, the algorithm sets the
+/// floating-point value of the sampel to 0.0.
 pub fn process_algorithm(buffer: &mut Buffer) {
     for samples in buffer.iter_samples() {
         for sample in samples {
@@ -17,6 +21,10 @@ mod tests {
 
 
     #[test]
+    /// Create a two-channel buffer filled with 512 samples of value 5.0.
+    /// Test that this buffer has been validly created. Then apply the
+    /// processing algorithm to the buffer and verify that every sample in
+    /// the modified buffer is 0.0.
     fn mute_buffer_512_long() {
         let sample_init_val = 5.0;
 
@@ -43,6 +51,11 @@ mod tests {
     }
 
     #[test]
+    /// Create a two-channel buffer of length 0 - that is, the buffer should be
+    /// completely empty.
+    /// Test that this buffer has been validly created. Then apply the
+    /// processing algorithm to the buffer and verify that every sample in
+    /// the modified buffer is 0.0, and that the buffer contains no samples.
     fn mute_buffer_0_long() {
         let sample_init_val = 5.0;
 
@@ -66,9 +79,16 @@ mod tests {
                 assert_eq!(*sample, 0.0);
             }
         }
+
+        // Verify that the buffer is length 0
+        assert_eq!(buffer.iter_samples().len(), 0);
     }
 
     #[test]
+    /// Create a two-channel buffer filled with 1024 samples of value 5.0.
+    /// Test that this buffer has been validly created. Then apply the
+    /// processing algorithm to the buffer and verify that every sample in
+    /// the modified buffer is 0.0.
     fn mute_buffer_1024_long() {
         let sample_init_val = 34.0;
 
